d4c317003c289f53616e5f00aa338c2a879e0f0c
Ace <ajovanov.time.mk@gmail.com>
2017-09-21 19:13:44 +0200

    Trying out joins and enum instances

--- src/Data/Frame/Join.hs
+++ src/Data/Frame/Join.hs
@@ -1,3 +1,23 @@
 module Data.Frame.Join where
 
+import qualified Data.Vector as V
+
+import qualified Data.Discrimination as D
+import qualified Data.Discrimination.Grouping as DG
+
+-- Theoretically will exist one day
+-- import Labels(get, recordAppend)
+
+discriminator = DG.hashing
+
+-- https://github.com/Gabriel439/slides/blob/master/lambdaconf/data/exercises/08/Main.hs
 -- https://hackage.haskell.org/package/discrimination-0.2.1/docs/Data-Discrimination.html
+-- D.inner (DG.hashing) (\(a, b) (c, d) -> (a, b, c, d)) fst snd [(1, 2), (2, 3)] [(5, 1), (3, 2)]
+
+-- innerJoin df1 df2 lbl1 lbl2 = V.fromList . head $ D.inner discriminator combiner p1 p2 dat1 dat2
+-- 	where
+-- 		combiner = \r1 r2 -> recordAppend r1 r2
+-- 		p1       = get lbl1
+-- 		p2       = get lbl2
+-- 		dat1     = V.toList df1
+-- 		dat2     = V.toList df2
