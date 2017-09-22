6261c9540e77d4a686e188c5418567f7cc90bc64
Aleksandar Jovanov <aleksandar.jovanov.1995@gmail.com>
2017-09-22 16:53:35 +0200

    updated readme to contain todos and fixed enum instances for product types

--- README.md
+++ README.md
@@ -10,3 +10,14 @@ More precisely the library aims to provide:
 - Plotting framework inspired by ggplot (In progress)
 
 Take a look at Main.hs for some examples of analysing the iris dataset. 
+
+# TODO
+ The following things need to be added to Labels so DST can progress
+ > drop field from record = drop #age (#age := 1, #blabla := ":)") gives (#blabla := ":)"), project can do this but it requires a type anotation
+ > append two records     = append (#age := 1, #bla := ")") (#name := "User", #lbl := "Hey there") 
+                            gives (#age := 1, #bla := ")", #name := "User", #lbl := "Hey there")
+ > easy type casts        = cast #value @Integer (#value := 1.3, #test = 1.5) = (#value := 1, #test = 1.5) 
+                            or change modify (value -> value) to (value1 -> value2)
+
+They hold back joins, binData, mergeColumns and dropColumns
+Labels.Cassava.Instances needs ToNamedRecord instances for writeCsv
--- src/Data/Frame/Instances.hs
+++ src/Data/Frame/Instances.hs
@@ -1,32 +1,35 @@
-{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances #-}
 module Data.Frame.Instances where
 
 -- We need this to allow grouping with columns that contain missing values
 -- Nothing is a special group 0 while Just values are a group each.
 instance (Enum a) => Enum (Maybe a) where
   fromEnum Nothing  = 0
-  fromEnum (Just x) = 1 + (fromEnum x)
+  fromEnum (Just x) = succ . fromEnum $ x
 
   toEnum 0 = Nothing
-  toEnum x = Just $ (toEnum (x - 1))
+  toEnum x = Just . toEnum . pred $ x
 
--- FIXME: Malformed instance: (Enum a, Enum b) => (a, b) What does this even mean?
 -- We need this to allow grouping with composite keys. Composite key = tuple valued column.
--- How many unique values can each tuple component contain. 
--- constK = 30000
+-- How many unique values can each tuple component contain.
+-- Valeus above constK will overflow and information will be lost.
+constK = 30000
 
--- instance (Enum a, Enum b) => (a, b) where
--- 	fromEnum (a, b) = a * constK + (b `mod` constK)
--- 	toEnum x        = (x `div` constK, x `mod` constK)
+instance (Enum a, Enum b) => Enum (a, b) where
+    fromEnum (a, b) = (fromEnum a) * constK + ((fromEnum b) `mod` constK)
 
--- instance (Enum a, Enum b, Enum c) => (a, b, c) where
--- 	fromEnum (a, b, c) = fromEnum (a, b) * constK + (c `mod` constK)
--- 	toEnum   x         = (a, b, c)
--- 		where
--- 			ab = x  `div` constK
--- 			c  = x  `mod` constK
--- 			a  = ab `div` constK
--- 			b  = ab `mod` constK
+    toEnum x        =  (a', b')
+        where
+            a' = toEnum (x `div` constK)
+            b' = toEnum (x `mod` constK)
+
+instance (Enum a, Enum b, Enum c) => Enum (a, b, c) where
+ fromEnum (a, b, c) = fromEnum (a, b) * constK + ((fromEnum c) `mod` constK)
+ toEnum   x         = (toEnum a', toEnum b', toEnum c')
+     where
+         ab = x  `div` constK
+         c' = x  `mod` constK
+         a' = ab `div` constK
+         b' = ab `mod` constK
 
 
 -- Actually this led me to the following question.
@@ -38,3 +41,4 @@ instance (Enum a) => Enum (Maybe a) where
 -- For example the above property is not satisfied by regular multiplication (* = regular positve integer multiplication).
 -- If we take f1 = 3* and f2 = 6* then f1 4 = 12 = f2 2 so there is no way to know if A = 3 and B = 4 or A = 6 and B = 2.
 -- The goal is to have a unique as defined mapping from (A, B) to Z+ via (toEnum A * toEnum B)
+-- Overwriting mappings like hashing don't fit in.
